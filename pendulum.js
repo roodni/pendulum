e013cae621d50aba7d9f302fbcc3e41d7f10dccb
rodoni <41773436+roodni@users.noreply.github.com>
2018-07-26 18:59:22 +0900

    力学的エネルギーを表示

--- pendulum.js
+++ pendulum.js
@@ -1,21 +1,21 @@
 class Pendulum {
     init(images) {
-        this.revolution = true;
+        this.revolution = false;
         this.images = images;
 
-        this.num = 3;
+        this.num = 8;
         this.mass = new Array(this.num).fill(10);
-        let l = 200;
+        let l = 50;
         this.len = new Array(this.num).fill(l);
         this.lenG = new Array(this.num).fill(l);
-        this.rG = new Array(this.num).fill(0);
+        this.rG = new Array(this.num).fill(Math.PI / 180 * 0);
 
         this.I = [];
         for (let i = 0; i < this.num; i++) {
             this.I[i] = 0;//this.mass[i] * Math.pow(this.len[i], 2) / 12;
         }
 
-        let r = new Array(this.num).fill(Math.PI / 180 * 45);
+        let r = new Array(this.num).fill(Math.PI / 180 * 60);
         let v = new Array(this.num).fill(0);
         this.vec = new Vector(r.concat(v));
 
@@ -30,6 +30,44 @@ class Pendulum {
         for (let i = 0; i < this.num; i++) {
             this._massSum[i + 1] = this._massSum[i] + this.mass[i];
         }
+
+        this.firstE = this.getE();
+    }
+    getE() {
+        //運動エネルギー
+        let T = 0;
+        let sum2 = 0;
+        let getSum2elm = (i, j) => {
+            return this.len[i] * this.len[j] * this.vec.elm[this.num + i] * this.vec.elm[this.num + j]
+                * Math.cos(this.vec.elm[i] - this.vec.elm[j] + this.rG[i] - this.rG[j]);
+        }
+        for (let k = 0; k < this.num; k++) {
+            let sum1 = 0;
+            for (let i = 0; i < k; i++) {
+                sum1 += this.len[i] * this.vec.elm[this.num + i] * Math.cos(this.vec.elm[i] + this.rG[i] - this.vec.elm[k]);
+            }
+
+            for (let i = 0; i < k - 1; i++) {
+                sum2 += getSum2elm(i, k - 1) + getSum2elm(k - 1, i);
+            }
+            if (k > 0) {
+                sum2 += getSum2elm(k - 1, k - 1);
+            }
+
+            T += (this.mass[k] * Math.pow(this.lenG[k], 2) + this.I[k]) * Math.pow(this.vec.elm[this.num + k], 2) / 2
+                + this.mass[k] * (this.lenG[k] * this.vec.elm[this.num + k] * sum1 + sum2 / 2);
+        }
+
+        //ポテンシャル
+        let U = 0;
+        let sum = 0;
+        for (let k = 0; k < this.num; k++) {
+            if (k > 0) {
+                sum += this.len[k - 1] * (this.gx * Math.sin(this.vec.elm[k - 1] + this.rG[k - 1]) + this.gy * Math.cos(this.vec.elm[k - 1] + this.rG[k - 1]));
+            }
+            U += -this.mass[k] * (sum + this.lenG[k] * (this.gx * Math.sin(this.vec.elm[k]) + this.gy * Math.cos(this.vec.elm[k])));
+        }
+        return T + U;
     }
     massSum(begin, end) {
         return this._massSum[end + 1] - this._massSum[begin];
@@ -73,21 +111,26 @@ class Pendulum {
     update() {
         let dt = 1 / this.loop;
         for (let i = 0; i < this.loop; i++) {
+            //オイラー法
+            //this.vec = this.vec.add(this.bibun(this.vec).mult(dt));
+
+            //修正オイラー法
             let tmp1 = this.bibun(this.vec).mult(dt);
             let tmp2 = this.bibun(this.vec.add(tmp1)).mult(dt);
             this.vec = this.vec.add(tmp1.add(tmp2).mult(1 / 2));
         }
     }
+    
     draw(ctx) {
         let x = this.ox;
         let y = this.oy;
         for (let i = 0; i < this.num; i++) {
-            let dx = this.len[i] * Math.sin(this.vec.elm[i]);
-            let dy = this.len[i] * Math.cos(this.vec.elm[i]);
+            let dx = this.len[i] * Math.sin(this.vec.elm[i] + this.rG[i]);
+            let dy = this.len[i] * Math.cos(this.vec.elm[i] + this.rG[i]);
 
             if (!this.revolution) {
                 ctx.strokeStyle = "#000000";
-                ctx.lineWidth = 10;
+                ctx.lineWidth = 20;
                 ctx.beginPath();
                 ctx.moveTo(x, y);
                 ctx.lineTo(x + dx, y + dy);
@@ -95,7 +138,7 @@ class Pendulum {
 
                 ctx.fillStyle = "#000000";
                 ctx.beginPath();
-                ctx.arc(x + dx, y + dy, 20, 0, Math.PI * 2);
+                ctx.arc(x + this.lenG[i] * Math.sin(this.vec.elm[i]), y + this.lenG[i] * Math.cos(this.vec.elm[i]), 20, 0, Math.PI * 2);
                 ctx.fill();
 
             } else {
@@ -121,6 +164,8 @@ class Pendulum {
         ctx.fillStyle = "#000000";
         ctx.font = "30px serif";
         ctx.textBaseline = "top";
-        ctx.fillText("絶起！w", 0, 0);
+        ctx.textAlign = "right";
+        //ctx.fillText("絶起！w", 0, 0);
+        ctx.fillText((this.getE() - this.firstE).toFixed(5), W, 0);
     }
 }
